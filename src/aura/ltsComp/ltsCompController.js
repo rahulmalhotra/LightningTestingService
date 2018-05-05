8727149c4f9ad3d499c4375aa8ffc046691e9140
Rahul Malhotra <rahulmalhotra@codeinow.com>
2018-05-05 09:40:40 +0530

    Demo part 2 added function call using aura:method

--- src/aura/ltsComp/ltsCompController.js
+++ src/aura/ltsComp/ltsCompController.js
@@ -1,5 +1,11 @@
 ({
 	doInit: function(component, event, helper) {
 		component.set('v.myName', 'Rahul Malhotra');
+	},
+
+	buttonClicked: function(component, event, helper) {
+		var name = component.get('v.myName');
+		name = name + ' - You are Awesome';
+		component.set('v.myName', name);
 	}
 })
