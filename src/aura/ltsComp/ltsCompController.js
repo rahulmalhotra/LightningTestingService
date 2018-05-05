833dae3d64f0b9c8cae138889bc4c606723ce48f
Rahul Malhotra <rahulmalhotra@codeinow.com>
2018-05-05 11:24:31 +0530

    Part 3 Completed - Server Side Callback Added

--- src/aura/ltsComp/ltsCompController.js
+++ src/aura/ltsComp/ltsCompController.js
@@ -7,5 +7,9 @@
 		var name = component.get('v.myName');
 		name = name + ' - You are Awesome';
 		component.set('v.myName', name);
+	},
+
+	fetchAccounts: function(component, event, helper) {
+		helper.fetchAccountsFromServer(component, event, helper);
 	}
 })
--- src/aura/ltsComp/ltsCompHelper.js
+++ src/aura/ltsComp/ltsCompHelper.js
@@ -1,5 +1,14 @@
 ({
-	helperMethod : function() {
-		
+	fetchAccountsFromServer : function(component, event, helper) {
+		var accountsAction = component.get('c.getAccountsList');
+		accountsAction.setCallback(this, function(response) {
+			var state = response.getState();
+			if(state==='SUCCESS') {
+				var accountsString = response.getReturnValue();
+				var accountsList = JSON.parse(accountsString);
+				component.set('v.accountsList', accountsList);
+			}
+		});
+		$A.enqueueAction(accountsAction);		
 	}
 })
